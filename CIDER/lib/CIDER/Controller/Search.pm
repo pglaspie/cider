2dd31eec0d456b8a842ed7909b8df8ad5cd62b3b
Jason Macintosh <jmac@jmac.org>
2011-07-04 19:44:19 +0000

    #18 - Adding support for turning search results into sets.

--- CIDER/lib/CIDER/Controller/Search.pm
+++ CIDER/lib/CIDER/Controller/Search.pm
@@ -66,7 +66,61 @@ sub search :Path :Args(0) :FormConfig {
         $c->stash->{ objects } = \@objects;
         $c->stash->{ query } = $query_str;
         $c->stash->{ template } = 'search/results.tt';
+
+        # The results page has a create-a-new-set form on it.
+        my $set_creation_form = $self->form;
+        $set_creation_form->
+            load_config_filestem( 'set/create' );
+        $c->stash->{ set_creation_form } = $set_creation_form;
+
+        # Aim this form at the set-creation action.
+        $set_creation_form->action( '/search/create_set' );
+     
+        # Stick the last query in the flash, for the create-set action's use.
+        $c->flash->{ last_search_query } = $query;
+        
     }
+}
+
+=head2 create_set
+
+Path: /search/create_set
+
+This action is called if the user asks to create a new set from search
+results. It creates the set, and then sends the user along to the set list.
+
+=cut
+
+# /search/create_set: Create a new set based on the results of the last
+#                     query.
+sub create_set :Path('create_set') :Args(0) :FormConfig('set/create') {
+    my ( $self, $c ) = @_;
+
+    my $form = $c->stash->{ form };
+    
+    # Create the set object.
+    my $name = $form->param( 'name' );
+    my $rs = $c->model( 'CIDERDB::ObjectSet' );
+    my $set = $rs->create( {
+        name => $name,
+        owner => $c->user->id,
+    } );
+
+    # Populate the new set with the results of the last query.
+    my $query = $c->flash->{ last_search_query };
+
+    my $hits = $c->model( 'Search' )->search(
+        query => $query,
+    );
+
+    while ( my $hit = $hits->next ) {
+        my $object = $c->model( 'CIDERDB::Object' )->find( $hit->{id} );
+        $set->add ( $object );
+    }
+
+    # That done, redirect the user to the set list.
+    $c->flash->{ set_created_from_search } = $name;
+    $c->res->redirect( $c->uri_for( '/set/list' ) );
 }
 
 =head2 make_index
