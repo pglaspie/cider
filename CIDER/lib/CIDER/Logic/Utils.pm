26a3e96bbb545af7917feccb12a83905e11b3ede
Deborah Kaplan <deborah.kaplan@tufts.edu>
2012-10-25 09:53:56 -0400

    DCA-975 adding licenses!

--- CIDER/lib/CIDER/Logic/Utils.pm
+++ CIDER/lib/CIDER/Logic/Utils.pm
@@ -28,4 +28,24 @@ sub iso_8601_date {
                        || ( $2 >= 1 && $2 <= 31 ) ) ) );
 }
 
+=head1 LICENSE
+
+Copyright 2012 Tufts University
+
+CIDER is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+CIDER is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with CIDER.  If not, see
+<http://www.gnu.org/licenses/>.
+
+=cut
+
 1;
